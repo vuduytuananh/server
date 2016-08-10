3f142b9e495d98004c9858e42bd0eb9305a7c717
m1n10n <vuduytuananh@Vus-MacBook-Air.local>
2016-08-10 02:37:45 +0200

    add retailers

--- routes/phanphoi.js
+++ routes/phanphoi.js
@@ -5,5 +5,23 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('phanphoi');
 });
+router.get('/hanoi', function(req, res, next) {
+  res.render('phanphoi_hanoi');
+});
+router.get('/haiduong', function(req, res, next) {
+  res.render('phanphoi_haiduong');
+});
+router.get('/hungyen', function(req, res, next) {
+  res.render('phanphoi_hungyen');
+});
+router.get('/haiphong', function(req, res, next) {
+  res.render('phanphoi_haiphong');
+});
+router.get('/quangninh', function(req, res, next) {
+  res.render('phanphoi_quangninh');
+});
+router.get('/hanam', function(req, res, next) {
+  res.render('phanphoi_hanam');
+});
 
 module.exports = router;
