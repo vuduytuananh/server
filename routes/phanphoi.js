27a613dc445d0709c83c8a80f777b114bd845bc7
m1n10n <vuduytuananh@Vus-MacBook-Air.local>
2016-09-04 02:09:44 +0200

    xxx

--- routes/phanphoi.js
+++ routes/phanphoi.js
@@ -23,5 +23,36 @@ router.get('/quangninh', function(req, res, next) {
 router.get('/hanam', function(req, res, next) {
   res.render('phanphoi_hanam');
 });
+router.get('/bacninh', function(req, res, next) {
+  res.render('phanphoi_bacninh');
+});
+router.get('/thaibinh', function(req, res, next) {
+  res.render('phanphoi_thaibinh');
+});
+router.get('/bacgiang', function(req, res, next) {
+  res.render('phanphoi_bacgiang');
+});
+router.get('/vinhphuc', function(req, res, next) {
+  res.render('phanphoi_vinhphuc');
+});
+router.get('/thainguyen', function(req, res, next) {
+  res.render('phanphoi_thainguyen');
+});
+router.get('/phutho', function(req, res, next) {
+  res.render('phanphoi_phutho');
+});
+router.get('/ninhbinh', function(req, res, next) {
+  res.render('phanphoi_ninhbinh');
+});
+router.get('/hoabinh', function(req, res, next) {
+  res.render('phanphoi_hoabinh');
+});
+router.get('/thanhhoa', function(req, res, next) {
+  res.render('phanphoi_thanhhoa');
+});
+router.get('/nghean', function(req, res, next) {
+  res.render('phanphoi_nghean');
+});
+
 
 module.exports = router;
